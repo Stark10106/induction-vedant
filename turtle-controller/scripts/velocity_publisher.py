68e8c46bd6ba99a7dd81a5dd8e8c6d9947f48cf2
Vedant Vakharia <intelligentvedant@gmail.com>
2025-06-30 20:02:26 +0530

    Create velocity_publisher.py

---
+++ turtle-controller/scripts/velocity_publisher.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class VelocityPublisher(Node):
+    def __init__(self):
+        super().__init__('velocity_publisher')
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = Twist()
+        msg.linear.x = 0.2
+        msg.angular.z = 0.5
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: linear.x=%f, angular.z=%f' % (msg.linear.x, msg.angular.z))
+
+def main(args=None):
+    rclpy.init(args=args)
+    velocity_publisher = VelocityPublisher()
+    rclpy.spin(velocity_publisher)
+    velocity_publisher.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
